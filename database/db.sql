fdc91db0ca241ce19960a9157e4c8c51ec453ad1
jdColonia <JuanDavidColoniaAldana@gmail.com>
2024-06-26 03:04:54 -0500

    feat: add USERS table to script

--- database/db.sql
+++ database/db.sql
@@ -1,3 +1,11 @@
 DROP DATABASE IF EXISTS system_pos_db;
 CREATE DATABASE system_pos_db;
 USE system_pos_db;
+-- Crear tabla para los usuarios del sistema
+CREATE TABLE USERS (
+    id INT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
