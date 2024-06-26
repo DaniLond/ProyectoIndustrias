2349c973717001cfc4881cc70089e83513624ab5
jdColonia <JuanDavidColoniaAldana@gmail.com>
2024-06-26 01:36:06 -0500

    feat: add CORS middleware to accept requests from specific origins

---
+++ system-pos-backend/src/middlewares/cors.js
@@ -0,0 +1,17 @@
+import cors from 'cors';
+
+const ACCEPTED_ORIGINS = ['http://localhost:4000', 'http://localhost:5173'];
+
+const corsOptions = {
+	origin: (origin, callback) => {
+		if (!origin || ACCEPTED_ORIGINS.includes(origin)) {
+			callback(null, true);
+		} else {
+			callback(new Error('No permitido por CORS'));
+		}
+	},
+};
+
+const corsMiddleware = cors(corsOptions);
+
+export default corsMiddleware;
