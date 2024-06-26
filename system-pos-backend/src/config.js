d271db96e6e551505d7d68aaa1950fd5af1add1d
jdColonia <JuanDavidColoniaAldana@gmail.com>
2024-06-26 14:52:19 -0500

    feat: add new version of register users and preliminar version of login

--- system-pos-backend/src/config.js
+++ system-pos-backend/src/config.js
@@ -1,4 +1,7 @@
 export const PORT = process.env.PORT || 4000;
+export const SALT_ROUNDS = process.env.SALT_ROUNDS || 10;
+export const TOKEN_SECRET =
+	process.env.TOKEN_SECRET || 'a806P3dD1F4HIdik8FsuCr4wtIfFMko4eVYAp9tCDwWF4v6ijy5GfHrs6ci35aQx';
 export const DEFAULT_CONFIG = {
 	host: 'localhost',
 	user: 'industrias-londoño',
--- system-pos-backend/src/controllers/auth.controller.js
+++ system-pos-backend/src/controllers/auth.controller.js
@@ -1,9 +1,11 @@
 import bcrypt from 'bcryptjs';
 import { connectDB } from '../db.js';
+import { SALT_ROUNDS } from '../config.js';
+import { createAccessToken } from '../libs/jwt.js';
 
 export const register = async (req, res) => {
 	const { id, username, password, email } = req.body;
-	const hashedPassword = await bcrypt.hash(password, 10);
+	const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
 	try {
 		const connection = await connectDB();
@@ -14,13 +16,44 @@ export const register = async (req, res) => {
 			email,
 		]);
 
-		res.status(201).send({ message: 'Usuario registrado exitosamente' });
+		const token = await createAccessToken({ id: id, username: username });
+		res.cookie('token', token);
+		res.status(201).json({
+			message: 'Usuario registrado exitosamente',
+			user: { id, username, email },
+		});
 	} catch (error) {
 		res.status(500).send({ error: 'Error registrando el usuario' });
-		console.error(error);
+		console.error('Error registrando el usuario:', error);
 	}
 };
 
-export const login = (req, res) => {
-	res.send('login');
+export const login = async (req, res) => {
+	const { id, password } = req.body;
+
+	try {
+		const connection = await connectDB();
+		const [rows] = await connection.query('SELECT * FROM USERS WHERE id = ?;', [id]);
+
+		if (rows.length === 0) {
+			return res.status(400).send({ error: 'Usuario no encontrado' });
+		}
+
+		const user = rows[0];
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+
+		if (!isPasswordValid) {
+			return res.status(400).send({ error: 'Contraseña incorrecta' });
+		}
+
+		const token = await createAccessToken({ id: user.id, username: user.username });
+		res.cookie('token', token);
+		res.status(200).json({
+			message: 'Inicio de sesión exitoso',
+			user: { id: user.id, username: user.username, email: user.email },
+		});
+	} catch (error) {
+		res.status(500).send({ error: 'Error al iniciar sesión' });
+		console.error('Error al iniciar sesión:', error);
+	}
 };
