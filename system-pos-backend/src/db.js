7fa645b9cec0909a5a2a42956252ab1d4d52a1e0
jdColonia <JuanDavidColoniaAldana@gmail.com>
2024-06-28 03:54:13 -0500

    feat: add new configuration for database

--- system-pos-backend/src/db.js
+++ system-pos-backend/src/db.js
@@ -1,12 +1,21 @@
 import mysql from 'mysql2/promise';
 import { DEFAULT_CONFIG } from './config.js';
 
+// Configuración del pool de conexiones
+const pool = mysql.createPool({
+	...DEFAULT_CONFIG,
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0,
+});
+
 export const connectDB = async () => {
 	try {
-		const connection = await mysql.createConnection(DEFAULT_CONFIG);
+		const connection = await pool.getConnection();
 		console.log('Conexión exitosa a la base de datos MySQL');
 		return connection; // Retorna la conexión para poder usarla en otros lugares
 	} catch (error) {
 		console.error('Error conectándose a la base de datos:', error);
+		throw error; // Lanza el error para que pueda ser manejado en el lugar donde se llama a connectDB
 	}
 };
