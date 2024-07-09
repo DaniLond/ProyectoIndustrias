53d68204d43e42baa4c13795f16ce9bed1880203
jdColonia <JuanDavidColoniaAldana@gmail.com>
2024-07-09 03:57:41 -0500

    fix: correct configuration of error 404 page

--- system-pos-frontend/src/App.jsx
+++ system-pos-frontend/src/App.jsx
@@ -9,6 +9,7 @@ import RegisterPage from './pages/auth/RegisterPage';
 import LoginPage from './pages/auth/LoginPage';
 import ForgotPassword from './pages/auth/ForgotPassword';
 import ResetPassword from './pages/auth/ResetPassword';
+import Error404Page from './pages/Error404Page';
 
 // Rutas protegidas
 import { ProtectedRoute } from './routes';
@@ -25,10 +26,14 @@ function App() {
 						<Route path='/register' element={<RegisterPage />}></Route>
 						<Route path='/forgot-password' element={<ForgotPassword />}></Route>
 						<Route path='/reset-password/:token' element={<ResetPassword />}></Route>
+
 						{/* Rutas protegidas */}
 						<Route element={<ProtectedRoute />}>
 							<Route path='/home' element={<ProductPage />}></Route>
 						</Route>
+
+						{/* Ruta para manejo de errores 404 en rutas públicas */}
+						<Route path='*' element={<Error404Page />} />
 					</Routes>
 				</BrowserRouter>
 			</ProductProvider>
